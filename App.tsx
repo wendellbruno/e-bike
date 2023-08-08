dc5592c637faf00c093f35f611fd0dd508558c47
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2023-08-08 11:50:03 -0300

    ajustes
    ajustes

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider, Box } from "native-base";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { theme } from "./src/styles/theme";
+import {Home} from './src/screens';
 
 export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
       <Box flex="1" bg={theme.colors.primary[100]}>
-
+        <Home />
       </Box>
     </NativeBaseProvider>
   );
